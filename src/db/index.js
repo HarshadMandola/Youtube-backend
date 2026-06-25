88279b56d9950dcd6ab82810c7285907c7c12d55
HarshadMandola <mandolaharshad6@gmail.com>
2026-06-26 00:12:58 +0530

    all setup configured

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_Name } from "../constants.js";
 const connectDB= async ()=>{
     try {
         const ConnectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}`)
-        console.log('MongoDB Connected !!!!____--->',ConnectionInstance);
+        console.log('MongoDB Connected !!!!____--->'); //,ConnectionInstance
         
     } catch (error) {
         console.log('Connection faild',error);

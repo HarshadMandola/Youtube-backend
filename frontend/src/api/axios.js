089155929d1227c8e7cc5c35d1ba6ea1e749f2cb
HarshadMandola <mandolaharshad6@gmail.com>
2026-07-22 00:12:31 +0530

    add vercel base url for frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1", // match your backend's base URL
+  baseURL: "import.meta.env.VITE_API_BASE_URL", // match your backend's base URL
   withCredentials: true, // important if your backend uses cookies for JWT
 });
 
